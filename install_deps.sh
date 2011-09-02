c329d897a1e147d290e4a5b5d6521df2fe5e8fc0
John Eastman <john.eastman@gmail.com>
2011-09-02 12:11:49 -0500

    Added coffee-mode as submodule

--- install_deps.sh
+++ install_deps.sh
@@ -6,7 +6,6 @@ wget http://autopair.googlecode.com/svn/trunk/autopair.el
 wget http://www.northbound-train.com/emacs/dired-single.el
 wget http://jblevins.org/projects/markdown-mode/markdown-mode.el
 wget http://www.emacswiki.org/emacs/download/multi-term.el
-git clone git://github.com/defunkt/coffee-mode.git
 
 cd /tmp
 wget http://cx4a.org/pub/auto-complete/auto-complete-1.3.1.tar.bz2
