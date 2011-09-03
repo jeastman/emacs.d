0baede218140ee04f37df8e0b65fe0e1e3f49213
John Eastman <john.eastman@gmail.com>
2011-09-03 17:22:20 -0500

    Add pianobar as submodule and dired fix

--- init.el
+++ init.el
@@ -277,11 +277,11 @@
     "Bunch of stuff to run for dired, either immediately or when it's
   loaded."
     ;; <add other stuff here>
-    (define-key dired-mode-map [return] 'joc-dired-single-buffer)
-    (define-key dired-mode-map [mouse-1] 'joc-dired-single-buffer-mouse)
+    (define-key dired-mode-map [return] 'dired-single-buffer)
+    (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
     (define-key dired-mode-map "^"
       (function
-       (lambda nil (interactive) (joc-dired-single-buffer "..")))))
+       (lambda nil (interactive) (dired-single-buffer "..")))))
   ;; if dired's already loaded, then the keymap will be bound
   (if (boundp 'dired-mode-map)
       ;; we're good to go; just add our bindings
@@ -491,6 +491,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when (require 'rainbow-mode nil 'noerror)
   (add-hook 'css-mode-hook 'rainbow-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Pianobar
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(autoload 'pianobar "pianobar" nil t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Start the Server
