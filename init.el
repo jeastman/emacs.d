1a8165cb950578ebaff2d9f4f7f8d2c11e1ff0ea
John Eastman <john.eastman@gmail.com>
2017-11-02 16:39:15 -0500

    Modernize init.el script a bit.
     * Removed references to pallet and cask.
     * Rely more heavily on use-package

--- init.el
+++ init.el
@@ -13,39 +13,53 @@
 ;;; Code:
 ;;
 
-;; Use cask for package management
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-(package-initialize)
+;; Bling on Software tip about Garbage Collection
+;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
+;; Original source appears to be:
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+(let ((gc-cons-threshold most-positive-fixnum))
 
-;; Utilize pallet to keep cask in sync
-(unless (package-installed-p 'pallet)
-  (package-refresh-contents)
-  (package-install 'pallet))
-(pallet-mode t)
+  ;; Warn about using older versions of Emacs
+  ;; Nothing specific here, but this configuration is developed and maintained on
+  ;; version specified here.
+  (when (version< emacs-version "25")
+    (warn "This configuration expects Emacs version 25 or greater - you are running %s!"
+          emacs-version))
 
-;; Utilize use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-(custom-set-variables
- '(use-package-verbose t))
+  ;; require the package manager
+  (require 'package)
 
-;; Ensure we have loaded an org version
-(when (not (cl-remove-if-not
-            (lambda (p) (equal 'org (car p)))
-            package-alist))
-  (message "No org-mode package found; installing now...")
-  (package-refresh-contents)
-  (package-install 'org))
+  ;; Set some startup configuration values
+  (setq-default
+   load-prefer-newer t            ; load prefers the newest version of a file
+   package-enable-at-startup nil) ; Prevent packages from being activated after reading init
 
-;; load org and make sure we have the right version
-(require 'org)
-(when (string-match "^[1234567]" (org-version))
-  (warn "Org-Mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version)))
+  ;; initialize the ELPA repositories where packages are fetched
+  (setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                           ("melpa" . "https://melpa.org/packages/")
+                           ("gnu"   . "https://elpa.gnu.org/packages/")))
 
-;; load the literate init file
-(org-babel-load-file (expand-file-name "init-ext.org" user-emacs-directory))
+  (package-initialize)
+
+  ;; Utilize use-package
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+
+  (require 'use-package)
+  (custom-set-variables
+   '(use-package-verbose t))
+
+  ;; load org and make sure we have the right version
+  (use-package org
+    :ensure t
+    :pin org
+    :ensure org-plus-contrib)
+
+  ;; load the literate init file
+  (org-babel-load-file (expand-file-name "init-ext.org" user-emacs-directory))
+
+  ;; trigger garbage collection to clean up our deferred mess
+  (garbage-collect))
 (provide 'init)
 ;;; init.el ends here
