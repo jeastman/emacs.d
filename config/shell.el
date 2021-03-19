91e01128c1e8da210e58bea635dee12b2f435088
John Eastman <john.eastman@synacor.com>
2021-03-19 16:33:09 -0500

    tweak configuration handling for eshell

--- config/shell.el
+++ config/shell.el
@@ -24,18 +24,34 @@
 
 ;;; Code:
 
-(setq eshell-visual-commands
-      '("less" "tmux" "top" "bash" "zsh"))
+(defun jme:configure-eshell ()
+  "Configure eshell."
+  ;; save comman history
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+  ;; Truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
 
-(setq eshell-visual-subcommands
-      '(("git"
-         "diff" "df" "dc"
-         "show"
-         "log" "lg" "tree" "lol" "lola" "lala" "ltla" "ldla")))
+  (setq eshell-history-size         10000
+        eshell-buffer-maximum-lines 10000
+        eshell-hist-ignore-dups t
+        eshell-scroll-to-bottom-on-input t))
 
 (use-package eshell-git-prompt
+  :after eshell
   :config
   (eshell-git-prompt-use-theme 'powerline))
+
+(use-package eshell
+  :hook (eshell-first-time-mode . jme:configure-eshell)
+  :config
+  (with-eval-after-load 'esh-opt
+    (setq eshell-visual-commands
+        '("less" "tmux" "top" "bash" "zsh"))
+    (setq eshell-visual-subcommands
+          '(("git"
+             "diff" "df" "dc"
+             "show"
+             "log" "lg" "tree" "lol" "lola" "lala" "ltla" "ldla")))))
 
 (use-package tramp
   :custom
