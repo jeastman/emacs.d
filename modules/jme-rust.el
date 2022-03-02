34791c54e768c8188276fa869f01e8a48404a001
John Eastman <john.eastman@gmail.com>
2022-03-01 19:16:17 -0600

    ADD rust configuration

---
+++ modules/jme-rust.el
@@ -0,0 +1,45 @@
+;;; jme-rust.el --- Configuration for Rust programming  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  John Eastman
+
+;; Author: John Eastman <jeastman@miggle.lan>
+;; Keywords: convenience, languages
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provides configuration of `rustic' package for use with Rust programming.
+
+;;; Code:
+(require 'straight)
+(require 'jme-common)
+(straight-use-package 'rustic)
+
+(defun jme-rust--enable ()
+  "Enable configuration."
+  (custom-set-variables
+   '(rustic-lsp-client 'eglot))
+  (add-hook 'rustic-mode-hook #'jme-common-auto-save-on-compilation))
+
+(defun jme-rust--disable ()
+  "Disable configuration."
+  (remove-hook 'rustic-mode-hook #'jme-common-auto-save-on-compilation))
+
+(jme-common-defconfiguration jme-rust "Configuration for Rust programming")
+
+(provide 'jme-rust)
+;;; jme-rust.el ends here
